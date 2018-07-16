897f108ae34d9d7834b83f29fe2d0546a3414c38
chichivica <talalaev.i.a@yandex.ru>
2018-07-16 12:31:54 +0000

    high resolution train

--- examples/detector.c
+++ examples/detector.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <libgen.h>
 #include <layer.h>
+#include <utils.h>
 
 static int coco_ids[] = {1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,27,28,31,32,33,34,35,36,37,38,39,40,41,42,43,44,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,67,70,72,73,74,75,76,77,78,79,80,81,82,84,85,86,87,88,89,90};
 
@@ -67,8 +68,8 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
     while(get_current_batch(net) < net->max_batches){
         if(l.random && count++%10 == 0){
             printf("Resizing\n");
-            int dim = (rand() % 10 + 10) * 32;
-            if (get_current_batch(net)+200 > net->max_batches) dim = 608;
+            int dim = rand_int(18, 22) * 32; //576,704
+            if (get_current_batch(net)+200 > net->max_batches) dim = 704;
             //int dim = (rand() % 4 + 16) * 32;
             printf("%d\n", dim);
             args.w = dim;
